d651d0ae9b156694fe0f386420ebd29e31c6981f
tolga bolen <tolgabolen@hotmail.com>
2022-08-27 21:36:45 -0400

    added more work

--- src/work1/HelloMelo.java
+++ src/work1/HelloMelo.java
@@ -3,5 +3,6 @@ package work1;
 public class HelloMelo {
     public static void main(String[] args) {
         System.out.println("work1 started");
+        System.out.println("created git repository");
     }
 }
